8da398da283b01f625e6656e1ed5581498edd295
duylinh13 <huynhleduylinh1311@gmail.com>
2024-09-14 10:50:28 +0700

    .

--- src/components/ImageSection/ImageSection.jsx
+++ src/components/ImageSection/ImageSection.jsx
@@ -1,11 +1,20 @@
 import React from "react";
-
 import heroImage from "../../assets/images/00-img.png";
+import "./ImageSection.css";
 
 export default function ImageSection() {
+  const handleImageError = () => {
+    console.error("Hình ảnh không thể tải.");
+  };
+
   return (
-    <div>
-      <img src={heroImage} alt="heroImage" />
+    <div className="image-container">
+      <img
+        src={heroImage}
+        alt="HeroImage"
+        onError={handleImageError}
+        loading="lazy" // Cải thiện hiệu suất tải trang
+      />
     </div>
   );
 }
